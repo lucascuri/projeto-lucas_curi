9e9d3fde964a094e1293c87d3151736895c83b32
Realiza <realiza@Realiza-16.local>
2017-07-05 17:02:14 -0300

    alteracao do jquery

--- docs/social-network/structure/js/jLogin.js
+++ docs/social-network/structure/js/jLogin.js
@@ -1,42 +1,46 @@
+
 $("#formulario").on("submit", function() {
+	
 	var email = $("[name = email]").val();
 	var senha = $("[name = senha]").val();
 
+	// VALIDAÇÕES //
+
 	
 	if (email == "" || senha == "") {
-		alert("Digite seus dados corretamente");
-
-		if (email == "") {
+			if (email == "") {
 			$("[name = email]").addClass('erro');
 		} else {
 			$("[name = email]").removeClass('erro');
 		}
-
+		
 		if (senha == "") {
 			$("[name = senha]").addClass('erro');
 		} else {
 			$("[name = senha]").removeClass('erro');
 		}
-	return false;
+		return false;
 	}
 
-	
-	var data = {
+	var data = {	
 		email: email,
-		senha: senha
+		password: senha
 	};
 
-	$.ajax ({
+	$.ajax({
 		type: "post",
-		url: "http://192.168.20.91:8085/feeds",
+		url: "http://192.168.20.91:8085/login",
 		data: data,
-
-		success: function(retorno) {
-			alert("Registrado com sucesso");
-			$("#formulario") [0].reset();
-		}
-	});
-	return;
-
-	
+		success: function(res) {
+			alert("Login realizado com sucesso!");
+			
+		},
+		error: function(xhr) {
+			alert(xhr.responseJSON.error.message);
+		}	
 });
+	
+	return false;
+
+});
+
