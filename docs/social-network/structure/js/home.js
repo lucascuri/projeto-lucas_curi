b7da2294b2dfa6db5b7af53fa1ed1369f7b0a5e1
lucascuri1 <lucascurigomes10@gmail.com>
2017-07-17 03:25:43 -0300

    home

--- docs/social-network/structure/js/home.js
+++ docs/social-network/structure/js/home.js
@@ -10,7 +10,65 @@ function loadUserData() {
 
 }
 
+function loadFeeds() {
+	var wrapper = $("#feed-content");
+	user_id = userData.id;
+	$.ajax({
+		url: "http://realizadigital-api.nodo.cc/feeds/" + user_id,
+		type: "get",
+		success: function(res) {
+			var posts = res.posts;
+			var html = "";
+			if (posts.length === 0) {
+				html = "<div class='feed'><p>Não há novos posts no momento.</p></div>";
+			} else {
+				posts.reverse();
+				for (i=0; i<posts.length; i++) {
+					if (posts[i].liked) {
+						src = "img/icon-like-active.png";
+					} else {
+						src = "img/icon-like.png";
+					}
+					html = html + "<div class='feeds'><div class='feeds-inside'><h4 class='feed-name'>" + posts[i].first_name + " " + posts[i].last_name + "<p class='feed-texto'>" + posts[i].text + "</p><div class='likes'><span class='span-likes'>" + posts[i].likes + "</span><img src='" + src + "'></div></div></div>";
+				}
+			}
+			wrapper.html(html);
+		}
+	});
+}
+
+$("#formulario").on("submit", function() {
+	var textarea = $("textarea").val();
+	var email = userData.email;
+	var password = userData.password;
+	if (textarea === "" || textarea > 200) {
+		$("textarea").addClass("erro");
+		return false;
+	} else if (textarea.length > 200) {
+		alert("Seu post excede o máximo de 200 caracteres.");
+		return false;
+	} else {
+		$("textarea").removeClass("erro");
+	}
+	$.ajax({
+		type: "post",
+		url: "http://realizadigital-api.nodo.cc/feed",
+		data: {
+			text: texto,
+			email: email,
+			password: password
+		},
+		success: function (res) {
+			loadFeeds();
+			$("textarea").val("");
+		}
+	});
+	return false;
+});
+
+
 function init() {
 	loadUserData();
+	loadFeeds();
 }
 init();
